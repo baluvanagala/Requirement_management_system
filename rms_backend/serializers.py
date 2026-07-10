74eae08851762eb472961c6de356cb60cf5618e4
baluvanagala <baluvanagala66@gmail.com>
2026-07-10 11:27:05 +0530

    adding some validations

--- rms_backend/serializers.py
+++ rms_backend/serializers.py
@@ -1,37 +1,90 @@
 from rest_framework import serializers
 from .models import CustomUser
-from django.db.models import Q
+import re
 
 
 class LoginSerializer(serializers.Serializer):
-    email_or_phone=serializers.CharField()
-    password=serializers.CharField(write_only=True)
+    email_or_phone = serializers.CharField()
+    password = serializers.CharField(write_only=True)
 
     def validate(self, attrs):
-        email_or_phone=attrs.get('email_or_phone')
-        password=attrs.get('password')
+        allowed_fields = {"email_or_phone", "password"}
+        received_fields = set(self.initial_data.keys())
+
+        extra_fields = received_fields - allowed_fields
+
+        if extra_fields:
+            raise serializers.ValidationError(
+                {
+                    "extra_fields": f"{', '.join(extra_fields)} fields are not allowed: "
+                }
+            )
+
+        email_or_phone = attrs.get("email_or_phone")
+        password = attrs.get("password")
 
         if not email_or_phone:
             raise serializers.ValidationError(
-                {'email_or_phone':'Email or Password  is required.'}
+                {"email_or_phone": "Email or phone number is required."}
             )
+
         if not password:
             raise serializers.ValidationError(
-                {'email_or_phone': 'Password is required'}
+                {"password": "Password is required."}
             )
-        
-        user = CustomUser.objects.filter(
-            Q(email=email_or_phone) |
-            Q(mobile=email_or_phone)
-        ).first()
 
-        if not user:
+        user = None
+
+        # Email Login
+        if any(char.isalpha() for char in email_or_phone):
+
+            email_pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+
+            if not re.match(email_pattern, email_or_phone):
+                raise serializers.ValidationError(
+                    {"email_or_phone": "Please enter a valid email address."}
+                )
+
+            user = CustomUser.objects.filter(
+                email__iexact=email_or_phone
+            ).first()
+
+            if not user:
+                raise serializers.ValidationError(
+                    {"email_or_phone": "Email is not registered."}
+                )
+
+        # Phone Login
+        elif email_or_phone.isdigit():
+
+            if len(email_or_phone) != 10:
+                raise serializers.ValidationError(
+                    {
+                        "email_or_phone":
+                        "Please enter a valid 10-digit phone number."
+                    }
+                )
+
+            user = CustomUser.objects.filter(
+                mobile=email_or_phone
+            ).first()
+
+            if not user:
+                raise serializers.ValidationError(
+                    {"email_or_phone": "Phone number is not registered."}
+                )
+
+        else:
             raise serializers.ValidationError(
-                {"email_or_phone": "User not found."}
+                {
+                    "email_or_phone":
+                    "Please enter a valid email address or phone number."
+                }
             )
+
         if not user.is_active:
             raise serializers.ValidationError(
-                {'message':'Your account  is inactivated'}
+                {"message": "Your account is inactive."}
             )
 
         if not user.check_password(password):
@@ -39,5 +92,5 @@ class LoginSerializer(serializers.Serializer):
                 {"password": "Invalid password."}
             )
 
-        attrs['user'] = user
+        attrs["user"] = user
         return attrs
